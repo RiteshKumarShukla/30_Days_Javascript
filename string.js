b052b5a255b1231ee3a737299742546e86c56663
Ritesh Kumar Shukla <110231091+RiteshKumarShukla@users.noreply.github.com>
2022-10-11 13:22:27 +0530

    String

---
+++ string.js
@@ -0,0 +1,16 @@
+let space = ' '           // an empty space string
+let firstName = 'Ritesh'
+let lastName = 'Shukla'
+let country = 'India'
+let city = 'Lucknow'
+let language = 'JavaScript'
+let job = 'Web Developer'
+let quote = "The saying,'Seeing is Believing' is not correct in 2020."
+let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
+
+
+// let fullName = firstName + space + lastName; // concatenation, merging two string together.
+// console.log(fullName);
+
+let fullName =firstName + space + lastName
+let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
