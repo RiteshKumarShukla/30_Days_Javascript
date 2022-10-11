92bdc2fa0635cef5f8606d63e7f24e42ebd62c42
Ritesh Kumar Shukla <110231091+RiteshKumarShukla@users.noreply.github.com>
2022-10-11 13:20:03 +0530

    Basic Js variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+// Declaring different variables of different data types
+let firstName = 'Ritesh' // first name of a person
+let lastName = 'Shukla' // last name of a person
+let country = 'India' // country
+let city = 'Lucknow' //  city
+let age = 23 // age in years
+let isMarried = false;
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Ritesh', //name of a person
+  job = 'Web Developer',
+  live = 'Lucknow'
