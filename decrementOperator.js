e547893e3df13a8b0ec971c0aaf9ad1b624fdf1c
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2022-10-17 08:58:50 +0530

    Decrement Operator

---
+++ decrementOperator.js
@@ -0,0 +1,11 @@
+// In JavaScript we use the decrement operator to decrease a value stored in a variable. The decrement could be pre or post decrement. Let us see each of them:
+
+
+//preDecrement
+let count = 0
+console.log(--count) // -1
+console.log(count)  // -1
+
+//postDecrement
+console.log(count--) // 0
+console.log(count)   // -1
