1d1871f083ece8d0e4bd20d1623d489a02240b03
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2022-10-18 16:08:28 +0530

    array To string

---
+++ arrayToString.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.toString()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
