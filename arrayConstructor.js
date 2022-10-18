7603bb6f6ec1802756f6bde3a59da4e0f573c35f
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2022-10-18 16:07:17 +0530

    Array Constructor

---
+++ arrayConstructor.js
@@ -0,0 +1,5 @@
+const arr = Array() // creates an an empty array
+console.log(arr)
+
+const eightEmptyValues = Array(8) // it creates eight empty values
+console.log(eightEmptyValues) // [empty x 8]
