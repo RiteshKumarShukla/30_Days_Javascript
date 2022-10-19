91377522d60ebaeb3363bde535a93d1045372d99
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2022-10-19 20:19:28 +0530

    Add files via upload

---
+++ FunctionWithoutParameter&Return.js
@@ -0,0 +1,33 @@
+// function without parameter,  a function which make a number square
+function square() {
+    let num = 2
+    let sq = num * num
+    console.log(sq)
+  }
+  
+  square() // 4
+  
+  // function without parameter
+  function addTwoNumbers() {
+    let numOne = 10
+    let numTwo = 20
+    let sum = numOne + numTwo
+  
+    console.log(sum)
+  }
+  
+  addTwoNumbers() // a function has to be called by its name to be executed 
+
+
+
+
+
+  function printFullName (){
+    let firstName = 'Asabeneh'
+    let lastName = 'Yetayeh'
+    let space = ' '
+    let fullName = firstName + space + lastName
+    console.log(fullName)
+}
+
+printFullName() // calling a function
---
+++ funWithUnlimitedParameters.js
@@ -0,0 +1,22 @@
+// / Let us access the arguments object
+​
+function sumAllNums() {
+ console.log(arguments)
+}
+
+sumAllNums(1, 2, 3, 4)
+// Arguments(4) [1, 2, 3, 4, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+
+/ function declaration
+​
+function sumAllNums() {
+  let sum = 0
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+  return sum
+}
+
+console.log(sumAllNums(1, 2, 3, 4)) // 10
+console.log(sumAllNums(10, 20, 13, 40, 10))  // 93
+console.log(sumAllNums(15, 20, 30, 25, 10, 33, 40))  // 173
